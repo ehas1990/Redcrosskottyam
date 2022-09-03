0bcbc22bb830671bdd3c11d291e0b853dc20df20
ehas1990 <ehas1990>
2022-09-03 10:28:02 +0530

    3-9-22 form correction

--- redcross/Become-an-intern.php
+++ redcross/Become-an-intern.php
@@ -4,22 +4,22 @@
 error_reporting(E_ERROR | E_PARSE);
 $name = $_POST['name'];
 $dob = $_POST['dob'];
-$phone= $_POST['phone'];
+$call= $_POST['call'];
 $qualification= $_POST['qualification'];
 $email=$_POST['email'];
 $address=$_POST['address'];
 $sub="Redcross Kottayam-Become An Intern";
-$to = "redcrosskottayam@gmail.com";  
-
-$txt ="Name : ". $name . "\r\n dob : " . $dob . "\r\n  phone :" . $phone ."\r\n  address :" . $address .  "\r\n qualification : . $qualification";
+// $to = "redcrosskottayam@gmail.com";  
+$to = "ehasalpha@gmail.com";  
+$txt ="Name : ". $name . "\r\n dob : " . $dob . "\r\n  contact Number :" . $call ."\r\n  address :" . $address .  "\r\n qualification : . $qualification";
 
 $headers = "From:" . $email;
  
 if(mail($to,$sub,$txt,$headers)) {
  
-echo "<h1>Sent Successfully! Thank you ".$name.", We will contact you shortly!</h1>";
+
 } else {
-    echo "The email message was not sent.";
+   
 }
 require_once "Become-an-intern.html";
 ?>
